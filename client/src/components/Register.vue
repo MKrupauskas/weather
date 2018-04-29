68899182f0bf30a040f5bb721be4ed07e3306994
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-04-29 20:18:49 +0300

    send register to server and setting up registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Weather</h1>
+    <!-- <p>{{ msg }}</p> -->
+    <label for="email">Email</label>
+    <input v-model="email" type="email" name="email" id="email" placeholder="awesome@email.com">
+    <label for="password">Password</label>
+    <input v-model="password" type="password" name="password" id="password" placeholder="Password">
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,11 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.send('hello world')
+app.post('/register', (req, res) => {
+  res.send({
+    message: 'hello world',
+    body: req.body
+  })
 })
 
 app.listen(process.env.PORT || 8081)
