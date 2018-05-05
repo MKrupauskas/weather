0bba93888051ef6ce58df38211dfa9465abcfbf5
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-05-05 23:45:10 +0300

    add a get

--- server/src/routes.js
+++ server/src/routes.js
@@ -6,4 +6,8 @@ module.exports = (app) => {
   app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
 
   app.post('/login', AuthenticationController.login)
+
+  app.post('/weather', (req, res) => {
+    res.send('testing')
+  })
 }
