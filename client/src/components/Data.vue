759a90304abc36371b3531f721afb6223508ecf9
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-05-16 21:27:09 +0300

    requesting data from server

--- client/src/components/Data.vue
+++ client/src/components/Data.vue
@@ -2,18 +2,30 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Data">
+        <div v-for="item in weather" :key="item.title">
+          {{ item.title }}
+          {{ item.description }}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import DataService from '@/services/DataService'
 import Panel from '@/components/Panel'
 export default {
   data () {
-    components: {
-      Panel
+    return {
+      weather: null
     }
+  },
+  async mounted () {
+    // make request to backend
+    this.weather = await DataService.index()
+  },
+  components: {
+    Panel
   }
 }
 </script>
